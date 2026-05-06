4c950a2e136213b5fe1586e104738d579bf6c1fe
bunkheangheng <bunkheangheng99@gmail.com>
2026-05-06 14:28:04 +0700

    Final Exam Prep

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 (function () {
   'use strict';
 
-  const QUIZ_FILES = ['quiz1', 'quiz2', 'quiz3', 'quiz4', 'quiz5', 'quiz6', 'quiz7'];
+  const QUIZ_FILES = ['quiz8', 'quiz9', 'quiz10', 'quiz11', 'quiz12', 'quiz13', 'quiz14'];
   const DATA_BASE = 'data';
   const CORRECT_PIN = '69696969';
   const PIN_STORAGE_KEY = 'quizPinUnlocked';
